dcef2be62aedef6ee9c8d52fdc4ed9c6ff31ef14
fvieir <fcvieira2@gmail.com>
2022-01-31 22:41:31 -0300

    Implementando token JWT-Auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        // autenticação por email e senha para depois liberar token
+        // retornar um token jwt
+        $credenciais = $request->all(['email', 'password']);
+
+        $token = auth('api')->attempt($credenciais);
+    
+        if ($token) {
+            // usuário autenticado
+            return \response()->json(['token' => $token], 200);
+        } else {
+            // erro de usuario ou senha
+            // 403 = forbidden => proibido (login invalido)
+            //401 = Unauthorized => Não autorizado
+            return \response()->json(['erro' => 'Usuário ou senha inválido'], 403);
+        }
+    
+        return 'login';
+    }
+
+    public function logout()
+    {
+        return 'logout';
+    }
+
+    public function refresh()
+    {
+        return 'refresh';
+    }
+   
+    public function me()
+    {
+        return 'me';
+    }
+}
