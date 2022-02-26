57c3932558f95a35cdc4bee8d645fc46b58baa8b
fvieir <fcvieira2@gmail.com>
2022-02-26 14:38:10 -0300

    Adicionando nav-item componente Vue para menu lado direito

---
+++ resources/js/components/menu/NavItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul class="nav ">
+        <li class="nav-item">
+            <a href="#" class="nav-link">Clientes</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">Locação</a>
+        </li>
+        <li class="nav-item dropdown">
+            <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Veículos</a>
+            <div class="dropdown-menu">
+                <a href="#" class="dropdown-item">Carros</a>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item">Marca</a>
+                <a href="#" class="dropdown-item">Modelo</a>
+            </div>
+        </li>
+    </ul>
+</template>
+
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <nav-item-component></nav-item-component>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
