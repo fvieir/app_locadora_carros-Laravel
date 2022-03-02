3ecfff65385ec8f825222016dbc2400280938d01
fvieir <fcvieira2@gmail.com>
2022-03-02 08:45:17 -0300

    Implementando componente table

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -42,35 +42,7 @@
                 <div class="card mt-4">
                     <div class="card-header">Relação de Marcas</div>
                     <div class="card-body">
-                        <table class="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">First</th>
-                                    <th scope="col">Last</th>
-                                    <th scope="col">Handle</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>Jacob</td>
-                                    <td>Thornton</td>
-                                    <td>@fat</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td colspan="2">Larry the Bird</td>
-                                    <td>@twitter</td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        <table-component></table-component>     
                     </div>
 
                     <div class="card-footer">
@@ -87,6 +59,7 @@
 <script>
 import InputContainer from './InputContainer.vue'
 export default {
-  components: { InputContainer },
+    name: 'Marcas',
+    components: { InputContainer },
 }
 </script>
